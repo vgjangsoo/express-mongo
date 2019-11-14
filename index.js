599131140beaf2125ed82232fb060d8773a1944c
Sungil Saul Lee <saul@Sungils-MacBook.local>
2019-11-13 17:45:19 -0800

    Install nodemon

--- index.js
+++ index.js
@@ -4,8 +4,15 @@ const path = require('path');
 
 const app = express();
 
+//Body Parser middelware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Set Static Path
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', function(req, res){
-    res.send("Hello World");
+    res.send("Hello");
 });
 
 app.listen(3000, function(){
