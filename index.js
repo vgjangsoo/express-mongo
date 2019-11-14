7388e7b53d0d149b30b94cd58e63c7745e1320b2
Sungil Saul Lee <saul@Sungils-MacBook.local>
2019-11-13 18:26:23 -0800

    showing users names on console

--- index.js
+++ index.js
@@ -15,8 +15,42 @@ app.use(bodyParser.urlencoded({extended: false}));
 // Set Static Path
 app.use(express.static(path.join(__dirname, 'public')));
 
+const users = [
+    {
+        id: 1,
+        first_name: 'John',
+        last_name: 'Doe',
+        email: 'johndoe@gmail.com'
+    },
+    {
+        id: 2,
+        first_name: 'Bob',
+        last_name: 'Smith',
+        email: 'bobsmith@gmail.com'
+    },
+    {
+        id: 3,
+        first_name: 'Jill',
+        last_name: 'Jackson',
+        email: 'jjackson@gmail.com'
+    }
+];
+
 app.get('/', function(req, res){
-    res.render('index');
+    res.render('index', {
+        title: 'Customers',
+        users: users
+    });
+});
+
+app.post('/users/add', function(req, res) {
+    const newUser = {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+    };
+
+    console.log(newUser);
 });
 
 app.listen(3000, function(){
